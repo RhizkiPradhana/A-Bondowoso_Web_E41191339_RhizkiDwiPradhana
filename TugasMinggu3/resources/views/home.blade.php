2086d8b84be93ab0a35857f09b9a9cb5f8eb1d4c
Rhizki Dwi Pradhana <dwipradhana14@gmail.com>
2021-03-25 13:06:18 +0700

    Tgs home

--- TugasMinggu3/resources/views/home.blade.php
+++ TugasMinggu3/resources/views/home.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name')}}</title>
-</head>
-<body>
-<div class="jumbotron jumbotron-fluid">
-    <div class="contaigner">
-    <h1 class="display-4">Halaman Home</h1>
-    <p class="lead">Halaman Ini Merupakan Halaman Home</p>
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron jumbotron-fluid">
+        <div class="contaigner">
+            <h1 class="display-4">Halaman Home</h1>
+            <p class="lead">This Is Home Page</p>
+        </div>
     </div>
-</body>
-</html>
+@endsection
